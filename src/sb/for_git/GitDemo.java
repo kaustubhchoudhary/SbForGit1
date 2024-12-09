f7d44848ca0b31a5002ba7ec241b255b8a0a5148
kaustubhchoudhary <kaustubhchoudhary@yahoo.com>
2024-12-09 16:48:07 +0530

    Implemented the functionality to fetch all users

--- src/sb/for_git/GitDemo.java
+++ src/sb/for_git/GitDemo.java
@@ -1,8 +1,20 @@
 package sb.for_git;
 
+import java.util.*;
+
 import sb.for_git.ex_1.Student;
 
 public class GitDemo {
+	
+	static List<Student> getStudents(){
+		
+		List<Student> students = new ArrayList<Student>();
+		
+		students.add(new Student(101, "Aakash"));
+		students.add(new Student(102, "Akshar"));
+		
+		return students;
+	}
 
 	public static void main(String[] args) {
 
